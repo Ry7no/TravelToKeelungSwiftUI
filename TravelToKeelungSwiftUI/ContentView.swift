d53e962d7b6bb83384c192e5d1f57ca29e489cc6
Ry7no <ry4novak@gmail.com>
2022-06-18 18:10:18 +0800

    Initial Commit

---
+++ TravelToKeelungSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TravelToKeelungSwiftUI
+//
+//  Created by Br7 on 2022/6/18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TravelToKeelungSwiftUI/TravelToKeelungSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelToKeelungSwiftUIApp.swift
+//  TravelToKeelungSwiftUI
+//
+//  Created by Br7 on 2022/6/18.
+//
+
+import SwiftUI
+
+@main
+struct TravelToKeelungSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
